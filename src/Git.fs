3a3b14cca7d4fc30acba787f8047310215652d3a
asti <asti@dynz.net>
2019-12-27 12:21:38 +0530

    Add gitconfig lookup

--- src/Git.fs
+++ src/Git.fs
@@ -1,8 +1,9 @@
 ﻿module Git
 
 open System
+open System.IO
 
-let binary = 
+let where = 
     let is_unix = Environment.OSVersion.Platform = PlatformID.Unix
     let path = Shell.where (if is_unix then "git" else "git.exe")
     match path with 
@@ -11,12 +12,13 @@ let binary =
 
 let private settings = Shell.Options.defaultSettings
 
-let git args path = Shell.create { settings with WorkingDirectory = Some(path) } binary args
+let git args path = Shell.create { settings with WorkingDirectory = Some(path) } where args
 
 let version () = git "--version" "."
 
 let pull = git "pull"
 let push = git "push"
 
-    
+let gitconfig dir = Path.Combine(dir, ".git", "config")
+let isGitDirectory = gitconfig >> File.Exists
 
