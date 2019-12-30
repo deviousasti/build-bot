729eb05caf59a0204f964665fba0da9f2bb4b814
asti <asti@dynz.net>
2019-12-30 18:53:44 +0530

    Add build pipeline creation

--- src/Build.fs
+++ src/Build.fs
@@ -1,11 +1,14 @@
 ﻿module Build
 
+open System
 open System.IO
+open FSharp.Control.Reactive
 
 type private GitRel = 
     { path: string; relative: string list; remote: string; }
 
 type Repository = { local: string; remote: string; root: string }
+type Graph = Map<string, seq<Repository>>
 
 let gitTree source =
     //recursively build dependency tree and 
@@ -45,11 +48,59 @@ let gitTree source =
             }
     }
     
-let gitGraph (workspace:string) =
+let gitGraph (workspace:string) : Graph =
     workspace.Split ';'
     |> Seq.collect(Directory.EnumerateDirectories)
     |> Seq.filter(Git.Paths.isGitDirectory)
     |> Seq.collect(gitTree)
-    |> Seq.groupBy(fun repo -> repo.remote)
+    |> Seq.groupBy(fun repo -> Git.rootUrl repo.remote)
     |> Map.ofSeq
 
+let matches (text:string) (graph:Graph) =
+    if (String.IsNullOrEmpty text) then 
+        Seq.empty
+    else
+        graph
+        |> Map.toSeq 
+        |> Seq.filter(fun (url, _) -> text.Contains(url, StringComparison.InvariantCultureIgnoreCase))
+        |> Seq.collect snd
+
+let urls (graph:Graph) = graph |> Map.toSeq |> Seq.map fst
+
+let update (repo:Repository) = Git.pull repo.local
+
+let getBuildTargets buildroot =
+    let file = Path.Combine(buildroot, "build")
+    let config = ConfigParser.tryParse file |> List.ofSeq
+    match config with 
+    | [] -> ["debug";"release"] |> List.map(fun t -> t, Map.empty)
+    | [x] -> x.Values |> Map.toList |> List.map (fun (_, value) -> value, Map.empty)
+    | many -> many |> List.map (fun section -> section.Name, section.Values)
+
+type Pipeline = string * (string -> IObservable<string>)
+
+let build (repo:Repository) =
+    let buildroot = repo.root 
+    let pipelines =
+        match buildroot with 
+        | Targets.Powershell x  -> [x]
+        | Targets.Bash x        -> [x]
+        | Targets.Command x     -> [x]
+        | Targets.MSBuild x     -> [x]
+        | Targets.Makefile x    -> [x]
+        | Targets.FAKE x        -> [x]
+        | Targets.CMake x       -> [x]
+        | Targets.Ninja x       -> [x]
+        | Targets.PlatformIO x  -> [x]
+        | Targets.Gulp x        -> [x]
+        | _ -> []
+
+    let targets = getBuildTargets buildroot
+    let settings = { Shell.Options.defaultSettings with WorkingDirectory = Some buildroot }
+    
+    seq {
+        for pipeline in pipelines do
+            for target, env in targets do
+                let settings' = { settings with EnvironmentVariables = env }
+                yield Pipeline(target, pipeline settings')
+    }
