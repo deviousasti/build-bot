b2127e53be0751935cf97353f2377d38d056f900
asti <asti@dynz.net>
2020-01-10 16:29:21 +0530

    Add build and deploy script

---
+++ deploy.ps1
@@ -0,0 +1,8 @@
+msbuild build-bot.sln
+choco pack
+
+$package = Get-ChildItem "*.nupkg" | Select-Object -First 1
+if($package -ne $null)
+{
+    choco push $package.Name --source https://push.chocolatey.org/
+}
